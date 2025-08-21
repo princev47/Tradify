708360858898f537021fa24292019cdfa907908c
princev47 <most.lyprince2004@gmail.com>
2025-08-21 18:01:51 +0530

    Add files via upload

---
+++ components/RootLayout.jsx
@@ -0,0 +1,42 @@
+"use client";
+
+import Footer from "./common/Footer";
+import { usePathname } from "next/navigation";
+import RootNav from "./common/RootNav";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import useAuth from "@/hooks/useAuth";
+import Loading from "./library/loading/Loading";
+
+const queryClient = new QueryClient();
+
+const MainLayout = ({ children }) => {
+  const { loading } = useAuth();
+  const pathname = usePathname();
+  const noHeaderFooter =
+    pathname.includes("login") ||
+    pathname.includes("register") ||
+    pathname.includes("dashboard") ||
+    pathname.includes("admin_dashboard") ||
+    pathname.includes("success");
+
+  if (loading && !noHeaderFooter) {
+    return (
+      <div className="h-screen w-full flex justify-center items-center bg-quaternary">
+        <Loading />
+      </div>
+    );
+  }
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <div>
+        {noHeaderFooter || <RootNav />}
+
+        {children}
+        {noHeaderFooter || <Footer />}
+      </div>
+    </QueryClientProvider>
+  );
+};
+
+export default MainLayout;
